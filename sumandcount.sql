38675c2d2c72aeb5755e98e6ce2d8baae97c8b08
Amir9eng <amirmukhtarme@gmail.com>
2021-01-10 18:21:29 +0100

    in-progress: sumandcount

---
+++ sumandcount.sql
@@ -0,0 +1,37 @@
+Total world population
+1.
+Show the total population of the world.
+
+world(name, continent, area, population, gdp)
+SELECT SUM(population)
+FROM world
+
+
+
+Correct answer
+SUM(population)
+7615648652
+
+
+
+
+List of continents
+2.
+List all the continents - just once each.
+
+SELECT DISTINCT continent 
+FROM world
+
+
+
+
+Correct answer
+continent
+Africa
+Asia
+Caribbean
+Eurasia
+Europe
+North America
+Oceania
+South America
